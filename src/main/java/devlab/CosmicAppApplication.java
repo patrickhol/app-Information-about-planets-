d6e45ee22cc37a1d3432f9aec1e02514de9a528a
Patrick Holody <patryk.holody@gmail.com>
2019-03-26 18:59:50 +0100

    init

---
+++ src/main/java/devlab/CosmicAppApplication.java
@@ -0,0 +1,13 @@
+package devlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosmicAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CosmicAppApplication.class, args);
+    }
+
+}
---
+++ src/main/java/devlab/ServletInitializer.java
@@ -0,0 +1,13 @@
+package devlab;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CosmicAppApplication.class);
+    }
+
+}
